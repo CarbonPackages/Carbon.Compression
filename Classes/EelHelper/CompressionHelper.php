2d5acf3d0067cb16d6af35cdc4dd02ec6c23e9d5
Günther Jedenastik <guenther@webandco.com>
2022-02-21 11:33:13 +0100

    ✨ Compress HTML on error pages too

---
+++ Classes/EelHelper/CompressionHelper.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Carbon\Compression\EelHelper;
+
+use WyriHaximus\HtmlCompress\Factory as HtmlCompress;
+use Neos\Eel\ProtectedContextAwareInterface;
+use Neos\Flow\Annotations as Flow;
+
+/**
+ * @Flow\Proxy(false)
+ */
+class CompressionHelper implements ProtectedContextAwareInterface
+{
+    /**
+     * Minify Html Content
+     *
+     * @param string $content
+     * @return string
+     */
+    public function compress(string $content): string
+    {
+        return HtmlCompress::construct()->compress($content);
+    }
+
+    /**
+     * All methods are considered safe
+     *
+     * @param string $methodName The name of the method
+     *
+     * @return bool
+     */
+    public function allowsCallOfMethod($methodName)
+    {
+        return true;
+    }
+}
